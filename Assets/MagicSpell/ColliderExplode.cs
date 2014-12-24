8453028dfd3c424c0c84ddcc9b95247271e29b35
ByzanTine <morbyzantine@gmail.com>
2014-12-24 06:49:13 -0500

    1. Use a dynamic link for the visual effect, easier to tweak with when change the visual effect. 2. some rename

--- Assets/MagicSpell/ColliderExplode.cs
+++ Assets/MagicSpell/ColliderExplode.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class ColliderExplode : MonoBehaviour {
 	public LayerMask overlapLayer;
+	public GameObject ExplodeEffect;
 	// Use this for initialization
 	void Start () {
 		
-	
-
+		
+		Instantiate (ExplodeEffect, transform.position, Quaternion.identity);;
 		Collider[] co = Physics.OverlapSphere(transform.position, 3.0f, overlapLayer);
 
 		// TODO Identify if the collider is pushable(namely only players)
