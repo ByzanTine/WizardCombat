90e0624cb5a8994ce10d408531cc0e5e6230e1e0
ByzanTine <morbyzantine@gmail.com>
2014-12-24 06:49:13 -0500

    1. Use a dynamic link for the visual effect, easier to tweak with when change the visual effect. 2. some rename

---
+++ Assets/EffectLink.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectLink : MonoBehaviour {
+	public GameObject visualEffect;
+	// Use this for initialization
+	void Start () {
+		GameObject gb = Instantiate (visualEffect, transform.position, transform.rotation) as GameObject;
+		gb.transform.parent = gameObject.transform;
+	}	
+	
+
+}
